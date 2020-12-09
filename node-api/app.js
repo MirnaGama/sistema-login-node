61b03255d807d616d000a23efb324b9c1c917ea4
Mirna Gama <mirnabeatrizgamav@gmail.com>
2020-12-09 16:00:38 -0300

    Correção - UPDATE e DELETE de Usuario

--- node-api/app.js
+++ node-api/app.js
@@ -42,12 +42,12 @@ app.route('/users/:id')
 	.catch(err => res.status(412))
 })
 .put((req, res) => { // UPDATE USER
-	Users.update({where: req.params})
+	Users.update(req.body, {where: {id: req.params.id}})
 	.then(result => res.json(result))
 	.catch(err => res.status(412))
 })
 .delete((req, res) => { // DELETE USER
-	Users.delete({where: req.params})
+	Users.destroy({where: req.params})
 	.then(result => res.status(204)) // SUCCESS STATUS
 	.catch(err => res.status(412))
 })
