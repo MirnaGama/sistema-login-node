9a208904e0538eef40985926988f19dbcdbb8829
Mirna Gama <mirnabeatrizgamav@gmail.com>
2020-12-10 18:49:35 -0300

    Implementando HttpStatus no controller

--- node-api/controllers/users.js
+++ node-api/controllers/users.js
@@ -1,5 +1,7 @@
-const defaultResponse = (data, statusCode = 200) => ({data, statusCode})
-const errorResponse = (message, statusCode = 400) => defaultResponse({error: message}, statusCode);
+import httpStatus from 'http-status'
+
+const defaultResponse = (data, statusCode = httpStatus.OK) => ({data, statusCode})
+const errorResponse = (message, statusCode = httpStatus.BAD_REQUEST) => defaultResponse({error: message}, statusCode);
 
 class UsersController {
     constructor(Users) {
@@ -18,18 +20,18 @@ class UsersController {
 
     // userData = request body
     createUser(userData) {
-        return this.Users.create(userData).then(result => defaultResponse(result, 201))
-        .catch(error => errorResponse(error.message, 422));
+        return this.Users.create(userData).then(result => defaultResponse(result, httpStatus.CREATED))
+        .catch(error => errorResponse(error.message, httpStatus.UNPROCESSABLE_ENTITY));
     }
 
     updateUser(userData, params) {
         return this.Users.update(userData, {where: {id: params.id}}).then(result => defaultResponse(result))
-        .catch(error => errorResponse(error.message, 422));
+        .catch(error => errorResponse(error.message, httpStatus.UNPROCESSABLE_ENTITY));
     }
 
     deleteUser(params) {
-        return this.Users.destroy({where: params}).then(result => defaultResponse(result, 204))
-        .catch(error => errorResponse(error.message, 422));
+        return this.Users.destroy({where: params}).then(result => defaultResponse(result, httpStatus.NO_CONTENT))
+        .catch(error => errorResponse(error.message, httpStatus.UNPROCESSABLE_ENTITY));
     }
 
 }
